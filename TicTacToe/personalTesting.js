198e280f6cbfb559a4e423395c49ad1f22c818e9
Billy Ngo <billyngo@talentpath.com>
2019-10-11 11:14:27 -0500

    restructure

--- TicTacToe/personalTesting.js
+++
@@ -1,8 +0,0 @@
-//make a string that repeats 5 times
-function printStrFiveTimes(string){
-    for(index = 0; index< 5; index++) {
-        console.log(string);
-    }
-}
-printStrFiveTimes("whatever you want it to be");
-
---
+++ personalTesting.js
@@ -0,0 +1,8 @@
+//make a string that repeats 5 times
+function printStrFiveTimes(string){
+    for(index = 0; index< 5; index++) {
+        console.log(string);
+    }
+}
+printStrFiveTimes("whatever you want it to be");
+
