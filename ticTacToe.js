76fbecc1d4cee27ad7f98d42256c63d8595648d5
Billy Ngo <billyngo@talentpath.com>
2019-10-22 17:50:00 -0500

    finish logic for TTT (excludes 5)

--- ticTacToe.js
+++ ticTacToe.js
@@ -16,31 +16,14 @@ class Board extends Array{
         }
     }
 
-    displayBoard()
-    {
-        let currentBoard = "\n"
-        let count = 0;
-        for(let index = 0; index < this.length; index++){
-            currentBoard += "| " + this[index] + " |";
-            count += 1; 
-            if(count === 3){
-                currentBoard += "\n================\n";
-                count = 0
-            }
-        }
-        return currentBoard;
-    }
-
     setPlayerTokens() {
         this.firPlayerToken = document.getElementById("player1").value;
         this.secPlayerToken = document.getElementById("player2").value;
         this.currentPlayer = this.firPlayerToken;
-        let curPlayer = document.getElementById("displayCurrentPlayer").innerText = "Current Player : " + this.currentPlayer;
-   
+        let curPlayer = document.getElementById("displayCurrentPlayer").innerText = "Current Player : " + this.currentPlayer;   
     }
     
     changeCurrentPlayer(){
-
         if (this.currentPlayer === this.firPlayerToken){
             this.currentPlayer = this.secPlayerToken;
         }
@@ -48,72 +31,83 @@ class Board extends Array{
             this.currentPlayer = this.firPlayerToken;
         }
         let curPlayer = document.getElementById("displayCurrentPlayer").innerText = "Current Player : " + this.currentPlayer;
-        this.displayMessage(this.currentPlayer + " you're up! Choose your spot");
     }
 
     displayMessage(message) {
         let changeMessage = document.getElementById("message").innerText = message;
     }
+
     checkWin(location){
+        //set Variables for starting points
+        let horiIndex = (location-1) % Math.floor(Math.sqrt(this.length));
+        let horiStartLoc = (location - horiIndex);
+        let vertStartLoc = location - horiStartLoc + 1;
+        let diagonalCheck = (Math.ceil(this.length/2) - location)
+        let centerPoint = diagonalCheck + location;
+        //Check 5
         
-        const halfWay = Math.ceil(this.length/2);
-        const sumLocHalf = halfWay-location;
-        //console.log(sumLocHalf + "and half way is " + halfWay);
-        let posiOfNum = Math.ceil(sumLocHalf+ location);
-        let winner = "";
-        //Evaluate for even numbers
-        if(location % 2 === 0){
-            //Check horizontal
-                //Check 4 and 6
-                let testVar = this[location].innerText;
-
-                console.log("testvar is " + this[posiOfNum-1] + " " + this[posiOfNum] + " " + this[posiOfNum+1]);
-                if(Math.abs(sumLocHalf) === 1) {
-                    this.hasWinner = true;
-                    if(this[posiOfNum-1] === this[posiOfNum] && this[posiOfNum] === this[posiOfNum+1]){
-                        this.displayMessage(this.currentPlayer + " is the winner!");
-                    }
-                }
-                else{
-                    
-                }
-                    
-            //Check Vertical
-        //console.log(this[location] + "is this location");
+        //Check Horizontal
+        if(this[horiStartLoc] === this[horiStartLoc+1] && this[horiStartLoc] === this[horiStartLoc+2])
+        {
+            this.hasWinner = true;
+            this.displayMessage(this.currentPlayer + " is the winner!");
         }
 
-        //evaluate for odd numbers
-        else if (location % 2 === 1){
-            //Check Horizontal
-            return
-            //check Vertical
-
-            //Check Diagonal
-
+        //Check Vertical
+        console.log(vertStartLoc + this[location] + " " + this[location+3] + " " + this[location+6]);
+        if(this[vertStartLoc] === this[vertStartLoc+3] && this[vertStartLoc] === this[vertStartLoc+6])
+        {
+            this.hasWinner = true;
+            this.displayMessage(this.currentPlayer + " is the winner!");
         }
 
-        //evaluate for 5
-        else{
-            return
+        //Check Diagonal
+        console.log(centerPoint + " " + (centerPoint + diagonalCheck) + " " + (centerPoint - diagonalCheck));
+        console.log(this[centerPoint] + " " + this[centerPoint + diagonalCheck] + " " + this[centerPoint - diagonalCheck])
+        if ((this[centerPoint] === this[centerPoint + diagonalCheck]) && (this[centerPoint] === this[centerPoint - diagonalCheck])) {
+            if (location === 5 && (centerPoint === centerPoint+4) && centerPoint === (centerPoint-4)) {
+                console.log("Execute 5" + centerPoint === (centerPoint + 4) && centerPoint === (centerPoint - 4));
+            }
+            else{
+                this.hasWinner = true;
+                this.displayMessage(this.currentPlayer + " is the winner!");
+            }
+            
         }
     }
 }
+
 
 const gameBoard = new Board();
 gameBoard.createTable();
 gameBoard.setPlayerTokens();
 let pieces = document.getElementById("1");
 
+//display Game Start
+gameBoard.displayMessage("Please type a token for player 1 and player 2");
+
+//Click event for board
+const start = document.getElementById("beginGame");
+start.onclick = function(){
+gameBoard.displayMessage("Game Start");    
+gameBoard.setPlayerTokens();   
+}
+
 //on click events for each element in the square
+
 for (let index = 1; index <=9; index++) {
     pieces = document.getElementById(index.toString());
     pieces.onclick= function(){
         //prevent selection if already selected
+        p1 = document.getElementById("player1").innerText;
+        p2 = document.getElementById("player2").innerText;
         if (this.innerText === gameBoard.firPlayerToken || this.innerText === gameBoard.secPlayerToken){
-            console.log("has an element already")
+            gameBoard.displayMessage("Tab is already Occupied, try again.")
         }
+        // if(p1 === "" || p2 === "") {
+        //     gameBoard.displayMessage("Please type a name for Player 1 and 2.")
+        // }
         else{
-            
             if(gameBoard.currentPlayer === gameBoard.firPlayerToken) {
                 this.style.backgroundColor = "blue";    
             }
@@ -123,18 +117,11 @@ for (let index = 1; index <=9; index++) {
             this.innerText = gameBoard.currentPlayer;
             gameBoard[index] = gameBoard.currentPlayer;
             gameBoard.checkWin(index);
-            gameBoard.changeCurrentPlayer();
+            gameBoard.changeCurrentPlayer();  
             
         }
-        
-
     };
-    console.log(gameBoard.currentPlayer + "is current player")
 }
 
 //make the on click event for player tokens
-const start = document.getElementById("beginGame");
-start.onclick = function(){
-gameBoard.setPlayerTokens();  
-//console.log(gameBoard.firPlayerToken)  
-}
+
